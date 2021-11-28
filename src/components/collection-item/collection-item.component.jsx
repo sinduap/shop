5d8575f9e8e567a53bb271b4348911c0b43fd319
Sindu Andita <sinduandita@gmail.com>
2021-11-29 03:19:11 +0700

    Add toggle cart to redux

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Button from "../button/button.component";
+
 import "./collection-item.styles.scss";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
@@ -11,6 +13,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <Button inverted>Add to cart</Button>
     </div>
   );
 };
