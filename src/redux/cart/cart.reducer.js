26c5920688dc2ead8bce4628c1c7d6e8127906aa
Sindu Andita <sinduandita@gmail.com>
2021-11-29 15:01:32 +0700

    Finished checkout page feature(add, remove, decrease item) logic

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,9 @@
 import { CartActionTypes } from "./cart.types";
-import { addItemToCart } from "./cart.utils";
+import {
+  addItemToCart,
+  removeItemFromCart,
+  decreaseItemFromCart,
+} from "./cart.utils";
 
 const INITIAL_STATE = {
   isHidden: true,
@@ -17,6 +21,16 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
+      };
+    case CartActionTypes.DECREASE_ITEM:
+      return {
+        ...state,
+        cartItems: decreaseItemFromCart(state.cartItems, action.payload),
       };
     default:
       return state;
