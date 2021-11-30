bf3249a5100517c09f0a6c640e508e17c80c1dbf
Sindu Andita <sinduandita@gmail.com>
2021-11-30 18:26:28 +0700

    Migrate remaining local state to redux, integrate redux-persist

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import reportWebVitals from "./reportWebVitals";
 
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Router>
   </Provider>,
   document.getElementById("root")
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
+import { connect } from "react-redux";
 
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-import SHOP_DATA from "./shop.data";
+import { selectShopDataCollections } from "../../redux/shop/shop.selectors";
 
-export class ShopPage extends Component {
-  constructor(props) {
-    super(props);
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+const mapStateToProps = (state) => ({
+  collections: selectShopDataCollections(state),
+});
 
-  render() {
-    const { collections } = this.state;
-
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
